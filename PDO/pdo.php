236c18c9b4f296d663eafd2728d3a40c04f4c0a1
1158806590 <1158806590@qq.com>
2019-01-19 11:26:39 +0800

    pdo类

--- PDO/pdo.php
+++ PDO/pdo.php
@@ -1,86 +1,131 @@
-
-    <?php   
-        header("Content-type:text/html; charset=utf-8");      
-        class DBPDO {  
+<?php   
+/** 
+    @DB Operates For PDO 
+    @author:MeeeeN 
+    @date:2015-10-22 22:40:32 
+**/  
+  
+    //定义数据库信息  
       
-            private static $instance;         
-            public $dsn;         
-            public $dbuser;         
-            public $dbpwd;         
-            public $sth;         
-            public $dbh;   
-            //初始化  
-            function __construct() {  
-                $this->dsn = 'mysql:host='localhost';dbname='admin;  
-                $this->dbuser = root;  
-                $this->dbpwd = 123456;  
-                $this->connect();  
-                $this->dbh->query("SET NAMES 'UTF8'");  
-                $this->dbh->query("SET TIME_ZONE = '+8:00'");  
+    header("Content-type:text/html; charset=utf-8");  
+  
+    define('DB_HOST', 'localhost');  
+    define('DB_USER', 'root');  
+    define('DB_PWD', '123456');  
+    define('DB_NAME', 'test');  
+  
+    class DBPDO {  
+  
+        private static $instance;         
+        public $dsn;         
+        public $dbuser;         
+        public $dbpwd;         
+        public $sth;         
+        public $dbh;   
+  
+        //初始化  
+        function __construct() {  
+            $this->dsn = 'mysql:host='.DB_HOST.';dbname='.DB_NAME;  
+            $this->dbuser = DB_USER;  
+            $this->dbpwd = DB_PWD;  
+            $this->connect();  
+            $this->dbh->query("SET NAMES 'UTF8'");  
+            $this->dbh->query("SET TIME_ZONE = '+8:00'");  
+        }  
+  
+        //连接数据库  
+        public function connect() {  
+            try {  
+                $this->dbh = new PDO($this->dsn, $this->dbuser, $this->dbpwd);  
             }  
-            //连接数据库  
-            public function connect() {  
-                try {  
-                    $this->dbh = new PDO($this->dsn, $this->dbuser, $this->dbpwd);  
-                }  
-                catch(PDOException $e) {  
-                    exit('连接失败:'.$e->getMessage());  
-                }  
-            }  
-            //插入数据  
-            public function insert($sql) {  
-                if($this->dbh->exec($sql)) {  
-                    $this->getPDOError();  
-                    return $this->dbh->lastInsertId();  
-                }  
-                return false;  
-            }  
-            //删除数据  
-            public function delete($sql) {  
-                if(($rows = $this->dbh->exec($sql)) > 0) {  
-                    $this->getPDOError();  
-                    return $rows;  
-                }  
-                else {  
-                    return false;  
-                }  
-            }  
-            //更改数据  
-            public function update($sql) {  
-                if(($rows = $this->dbh->exec($sql)) > 0) {  
-                    $this->getPDOError();  
-                    return $rows;  
-                }  
-                return false;  
-            }  
-      
-            //获取数据  
-            public function select($sql) {  
-                $this->sth = $this->dbh->query($sql);  
-                $this->getPDOError();  
-                $this->sth->setFetchMode(PDO::FETCH_ASSOC);  
-                $result = $this->sth->fetchAll();  
-                $this->sth = null;  
-                return $result;  
-            }  
-            //关闭连接  
-            public function __destruct() {  
-                $this->dbh = null;  
+            catch(PDOException $e) {  
+                exit('连接失败:'.$e->getMessage());  
             }  
         }  
+  
+        //获取表字段  
+        public function getFields($table='vista_order') {  
+            $this->sth = $this->dbh->query("DESCRIBE $table");  
+            $this->getPDOError();  
+            $this->sth->setFetchMode(PDO::FETCH_ASSOC);  
+            $result = $this->sth->fetchAll();  
+            $this->sth = null;  
+            return $result;  
+        }  
+  
+        //插入数据  
+        public function insert($sql) {  
+            if($this->dbh->exec($sql)) {  
+                $this->getPDOError();  
+                return $this->dbh->lastInsertId();  
+            }  
+            return false;  
+        }  
+  
+        //删除数据  
+        public function delete($sql) {  
+            if(($rows = $this->dbh->exec($sql)) > 0) {  
+                $this->getPDOError();  
+                return $rows;  
+            }  
+            else {  
+                return false;  
+            }  
+        }  
+  
+        //更改数据  
+        public function update($sql) {  
+            if(($rows = $this->dbh->exec($sql)) > 0) {  
+                $this->getPDOError();  
+                return $rows;  
+            }  
+            return false;  
+        }  
+  
+        //获取数据  
+        public function select($sql) {  
+            $this->sth = $this->dbh->query($sql);  
+            $this->getPDOError();  
+            $this->sth->setFetchMode(PDO::FETCH_ASSOC);  
+            $result = $this->sth->fetchAll();  
+            $this->sth = null;  
+            return $result;  
+        }  
+  
+        //获取数目  
+        public function count($sql) {  
+            $count = $this->dbh->query($sql);  
+            $this->getPDOError();  
+            return $count->fetchColumn();  
+        }  
+  
+        //获取PDO错误信息  
+        private function getPDOError() {  
+            if($this->dbh->errorCode() != '00000') {  
+                $error = $this->dbh->errorInfo();  
+                exit($error[2]);  
+            }  
+        }  
+  
+        //关闭连接  
+        public function __destruct() {  
+            $this->dbh = null;  
+        }  
+    }  
+  
+    //eg: an example for operate select  
+  
+    $test = new DBPDO;  
+  $aaa="杨赫";
+  $bbb="123";
+    $sql = "insert into user_info (user_name,user_age) values('$aaa','$bbb')";  
+  
+    $rs = $test->select($sql);  
+  
+    print_r($rs);  
       
-        //eg: an example for operate select  
-      
-        $test = new DBPDO;  
-      
-        $sql = "SELECT * FROM `user` WHERE `id`=1";  
-      
-        $rs = $test->select($sql);  
-      
-        print_r($rs);  
-          
-      
-      
-      
-      
-    ?>
+  
+  
+  
+  
+?>  
